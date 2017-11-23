8859758e40f5190d059cffdbc651ce6e341fc7c9
jancoow <jancokock@gmail.com>
2017-11-23 10:03:52 +0100

    Added PID state

--- src/MotorController.cpp
+++ src/MotorController.cpp
@@ -12,8 +12,26 @@ MotorController::MotorController(const Motor_Config *motor_config, const Encoder
 	this->encoder = new RotaryEncoder(encoder_config);
 	this->end_switch_1 = new LimitSwitch(end_switch_1);
 	this->end_switch_2 = new LimitSwitch(end_switch_2);
+	this->motorPid = new PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+
+	currentState = idle;
 }
 
+void MotorController::run(){
+	if(currentState == running){
+		//hardcoded 9000 steps
+        double motorSpeed = motorPid->calculate(9000, encoder->readSteps());
+        if(motorSpeed >= 0){
+        	motor->set_speed(motorSpeed);
+        	motor->forwards();
+        }else{
+        	motor->set_speed(-1*motorSpeed);
+        	motor->backwards();
+        }
+	}else if(currentState == calibrating){
+
+	}
+}
 
 
 
--- src/MotorController.h
+++ src/MotorController.h
@@ -11,15 +11,27 @@
 #include "Motor.h"
 #include "RotaryEncoder.h"
 #include "LimitSwitch.h"
+#include "Pid.h"
+
+enum MotorControllerState{
+	idle,
+	running,
+	calibrating,
+};
 
 class MotorController{
 public:
 	MotorController(const Motor_Config *motor_config, const Encoder_Config *encoder_config, const LimitSwitch_Config *end_switch_1, const LimitSwitch_Config *end_switch_2);
 
+	PID * motorPid;
 	Motor* motor;
 	RotaryEncoder* encoder;
 	LimitSwitch* end_switch_1;
 	LimitSwitch* end_switch_2;
+
+	void run();
+private:
+	MotorControllerState currentState;
 };
 
 #endif /* MOTORCONTROLLER_H_ */
